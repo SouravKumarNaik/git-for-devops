6ed7715af1fbb9c8b007ac9e3f5c4caf39476e80
SouravKumarNaik <souravkumrsitu@gmail.com>
2025-04-05 15:41:16 +0000

    Work in progress before rebase2

--- test.js
+++
@@ -1,9 +0,0 @@
-
-function sayHello(name) {
-    ```
-    Need to add this for testing purpose
-    ```
-    console.log("Hello, " + name)
-}
-
-sayHello("Sourav");
---
+++ test.py
@@ -0,0 +1,8 @@
+
+def sayHello(string):
+    ```
+    Need to add this for testing purpose
+    ```
+    print("help"+name)
+
+sayHello("name")
